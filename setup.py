c1ab84367741172956af5fbd85ca35c078e702be
Luca Fabbri <l.fabbri@bopen.eu>
2024-11-15 10:59:39 +0100

    Back to development: 0.3.1

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/keul/afk_slack_agent",
-    version="0.3.0",
+    version="0.3.1.dev0",
     zip_safe=False,
 )
